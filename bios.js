052e9a14c985df849ceb13f9ca51402f5afc69c3
Maryum Shabazz <49971083+Rebelworld@users.noreply.github.com>
2019-11-23 18:05:42 -0600

    Add files via upload

---
+++ bios.js
@@ -0,0 +1,7 @@
+
+//shows picture when button is clicked
+function popUp(){
+	var contentShow=document.getElementById("hiddenText");
+		contentShow.classList.remove("hidden");
+		contentShow.classList.add("visible");
+}
